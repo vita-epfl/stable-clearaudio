a9c80492388603be0316569891cd68bfeeefef64
AdrLfv <adrien.lefevre@epfl.fr>
2025-04-28 16:46:08 +0200

    Max_epoch is now configurable in the model config

--- train.py
+++ train.py
@@ -152,7 +152,7 @@ def main():
         callbacks=[ckpt_callback, demo_callback, exc_callback, save_model_config_callback],
         logger=logger,
         log_every_n_steps=1,
-        max_epochs=10000000,
+        max_epochs=model_config["training"]["max_epochs"],
         default_root_dir=args.save_dir,
         gradient_clip_val=args.gradient_clip_val,
         reload_dataloaders_every_n_epochs = 0,
