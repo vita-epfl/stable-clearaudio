a20ae0b013147ba2a830a9b3ece21882e202d743
maxencehof <maxence.hofer@epfl.ch>
2025-05-19 15:52:07 +0200

    corrected small mistake conditioners.py

--- stable_audio_tools/models/conditioners.py
+++ stable_audio_tools/models/conditioners.py
@@ -503,7 +503,7 @@ class PretransformConditioner(Conditioner):
         output_dim: the dimension of the output embeddings
     """
     def __init__(self, pretransform: Pretransform, output_dim: int, save_pretransform: bool = False):
-        super().__init__(pretransform.encoded_channels, output_dim))
+        super().__init__(pretransform.encoded_channels, output_dim)
 
         if not save_pretransform:
             self.__dict__["pretransform"] = pretransform
