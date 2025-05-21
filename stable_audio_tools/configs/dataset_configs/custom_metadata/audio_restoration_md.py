159781aba88d53ed44d32c35cc2b4c9b0d021021
AdrLfv <lefevre.adrien8@gmail.com>
2025-05-21 11:42:52 +0200

    Merged precomputed/on_the_fly metadata files in one

--- stable_audio_tools/configs/dataset_configs/custom_metadata/audio_restoration_md.py
+++ stable_audio_tools/configs/dataset_configs/custom_metadata/audio_restoration_md.py
@@ -1,9 +1,58 @@
 import torchaudio
 import torch
 import logging
+import os
+import random
+from pathlib import Path
+from stable_audio_tools.transforms import signal
+from omegaconf import OmegaConf
 
+LOG = logging.getLogger(__name__)
+LOG.addHandler(logging.StreamHandler())
+LOG.setLevel(logging.INFO)
 
 def get_custom_metadata(info, audio, args=None):
+    build_degraded = args.get("build_degraded", False)
+    if build_degraded:
+        return get_metadata_on_the_fly(info, audio, args)
+    elif not build_degraded:
+        return get_metadata_from_local(info, audio)
+    else:
+        raise ValueError("Invalid build_degraded value. Must be True or False")
+
+def get_metadata_on_the_fly(info, audio, args):
+    """
+    Maps clean audio files to their corresponding degraded versions using random mode.
+    
+    Args:
+        info (dict): Dictionary containing metadata about the audio file
+            info["total_length"] = audio.shape[-1]
+            info["path"] = audio_filename
+            info["timestamps"] = (t_start, t_end)
+            info["seconds_start"] = seconds_start
+            info["seconds_total"] = seconds_total
+            info["padding_mask"] = padding_mask
+            info["sample_rate"] = self.sr
+        audio (np.ndarray): The clean audio data
+        args (dict): Configuration parameters
+        
+    Returns:
+        dict: Dictionary containing the degraded audio data
+    """
+    
+    audio = signal.apply_config_to_audio(
+        info, 
+        audio, 
+        args["low_quality_effects_dir"],
+    )
+
+    # Return the degraded audio data
+    return {
+        "degraded_audio": audio
+    } 
+
+
+def get_metadata_from_local(info, audio):
     """
     Maps clean audio files to their corresponding degraded versions.
     
