ebeddd236f70d5fb0fc54d8f8d624cecce848a5f
AdrLfv <lefevre.adrien8@gmail.com>
2025-06-27 19:06:58 +0200

    Added cold_diffusion_cond case in create_diffusion_cond_from_config

--- stable_audio_tools/models/diffusion.py
+++ stable_audio_tools/models/diffusion.py
@@ -686,7 +686,7 @@ def create_diffusion_cond_from_config(config: tp.Dict[str, tp.Any]):
 
     extra_kwargs = {}
 
-    if model_type == "diffusion_cond" or model_type == "diffusion_cond_inpaint":
+    if model_type == "diffusion_cond" or model_type == "diffusion_cond_inpaint" or model_type == "cold_diffusion_cond":
         wrapper_fn = ConditionedDiffusionModelWrapper
 
         extra_kwargs["diffusion_objective"] = diffusion_objective
