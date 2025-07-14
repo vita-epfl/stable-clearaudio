2290e1b03792c37bbe5993530794f8baf0e468bc
AdrLfv <lefevre.adrien8@gmail.com>
2025-07-14 09:53:21 +0200

    Added t_start to restoration ui

--- stable_audio_tools/interface/interfaces/restoration.py
+++ stable_audio_tools/interface/interfaces/restoration.py
@@ -84,6 +84,7 @@ def generate_restoration(
     batch_size=1,
     degraded_audio_filename=None,
     custom_output_dir=None,
+    t_start=1.0,
 ):
     LOG.info("Starting audio restoration")
     
@@ -271,7 +272,8 @@ def generate_restoration(
             "degraded_audio": degraded_audio,
             "effects_list": effects_list,
             "output_dir": generation_dir,
-            "metrics_every": metrics_every
+            "metrics_every": metrics_every,
+            "t_start": t_start
         }
         audio, final_metrics = generate_cold_diffusion_uncond_restoration(**generate_args)
     else:
